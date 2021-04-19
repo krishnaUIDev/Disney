47ee8d01ef85015246b147049421438ffd517588
krishnaUIDev <krishna.kondoju007@gmail.com>
2021-04-18 23:28:57 -0400

    interfaces added

--- index.ts
+++ index.ts
@@ -1,11 +1,34 @@
 require("dotenv").config();
-import express, { Application } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import config from "config";
 
 const app: Application = express();
 
+// Rules of API
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Request-with, Content-Type, Accept, Authorization"
+  );
+  if (req.method == "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET PATCH DELETE POST PUT PATCH"
+    );
+    return res.status(200).json({});
+  }
+  next();
+});
+
 require("./server/startup/routes")(app);
 require("./server/startup/prod")(app);
+
+// ErrorHandling
+app.use((req: Request, res: Response, next: NextFunction) => {
+  const error: any = new Error("not Found");
+  return res.status(404).json({ message: error.message });
+});
 
 app.set("view engine", "pug");
 app.set("views", "./server/views"); // to set default template
